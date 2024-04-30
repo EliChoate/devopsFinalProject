0666261aceabade434dc5321e0d16d8182d1c36f
Eli <emchoate43@dev-ops.us-west4-b.c.spry-autumn-417919.internal>
2024-04-30 03:37:55 +0000

    final itteration of serverSetup.sh

--- serverSetup.sh
+++ serverSetup.sh
@@ -4,6 +4,8 @@ mkdir -p configurationLogs
 #ticketID and IPaddress as user inputs for this script
 intIPaddress=$1
 strTicketID=$2
+strHostname=$(hostname)
+touch ${strTicketID}.log
 #URL containing tickets
 strURL="https://www.swollenhippo.com/ServiceNow/systems/devTickets.php"
 #URL curled into a json file
@@ -19,10 +21,35 @@ strTicketIDlog=$(echo ${strCurledURL} | jq -r .[${intIndex}].ticketID)
 strRequestor=$(echo ${strCurledURL} | jq -r .[${intIndex}].requestor)
 strStandardConfig=$(echo ${strCurledURL} | jq -r .[${intIndex}].standardConfig)
 strDateTime=$(date +"%Y-%m-%d %H:%M:%S")
-for package in $(echo ${strCurledURL} | jq .[${intIndex}].softwarePackages); do
-	echo "hi: $package"
+echo -e "TicketID: ${strTicketID}\nStart DateTime: ${strDateTime}\nRequestor: ${strRequestor}\nExternal IP address: ${intIPaddress}\nHostname: ${strHostname}\nStandard Configuration: ${strStandardConfig}\n\n " >> ${strTicketID}.log
+for package in $(echo ${strCurledURL} | jq -r .[${intIndex}].softwarePackages[].install); do
+strPackageName=$(echo "${strCurledURL}" | jq -r .[${intIndex}].softwarePackages[].name)
+echo $strPackageName
+echo -e "softwarePackage -  ${strPackageName}" >> ${strTicketID}.log
+yes | sudo apt-get install ${package}
 done
-strSoftwarePackage=$(echo ${strCurledURL} | jq -r .[${intIndex}].softwarePackages)
+for config in $(echo "${strCurledURL}" | jq -r .[${intIndex}].additionalConfigs[].config); do
+strConfigName=$(echo "${strCurledURL}" | jq -r .[${intIndex}].additionalConfigs[].name)
+echo -e "\nadditional Config - ${strConfigName}" >> ${strTicketID}.log
+strConfig=$(echo "${strCurledURL}" | jq -r '.[].additionalConfigs[].config')
+if [[ ${config} == *"touch"* ]]; then
+strConfig=$(echo "${strCurledURL}" | jq -r '.['${intIndex}'].additionalConfigs[] | select(.config | contains("touch")) | .config')
+strTemp=$(echo "${strCurledURL}" | jq -r '.['${intIndex}'].additionalConfigs[] | select(.config | contains("touch")) | .config' | sed -n 's/.*\(\/var\/etc\/www\).*/\1/p')
+mkdir -p $strTemp
+fi
+#debugging
+echo "strConfigName: $strConfigName"
+echo "strTemp: $strTemp"
+echo "strConfig: $strConfig"
+echo "config: $config"
+eval "$strConfig"
+done
+for package in $(echo ${strCurledURL} | jq -r .[${intIndex}].softwarePackages[].install); do
+strPackageName=$(echo "${strCurledURL}" | jq -r .[${intIndex}].softwarePackages[].name)
+strVersionMsg=$(${package} -v)
+echo $strPackageName
+echo -e "Version - ${strPackageName} - ${strVersion}\n " >> ${strTicketID}.log
+done
 #debugging
 echo ${strTicketIDlog}
 echo ${strRequestor}
@@ -30,15 +57,8 @@ echo ${strStandardConfig}
 echo ${strDateTime}
 echo ${strSoftwarePackage}
 fi
-
-
-
-
-
-
-
 ((intIndex++))
 done
-
-
-
+strDateTime=$(date +"%Y-%m-%d %H:%M:%S")
+echo -e "\nTicket Closed\n" >> ${strTicketID}.log
+echo -e "Completed On: ${strDateTime}" >> ${strTicketID}.log
